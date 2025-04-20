260ae857a6151a69cffb166ff481779134c11338
Gaurav Jain <gaurjain14@gmail.com>
2025-04-19 22:53:15 -0700

    Rock-paper-scissors in JS

--- Front-end/the_odin_project/some-javascript/script.js
+++ Front-end/the_odin_project/some-javascript/script.js
@@ -1,6 +1,32 @@
-let firstname = "Helen";
-let surname = "Smith";
+getComputerChoice = () => { 
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
 
-console.log(firstname);
-console.log(surname);
-console.log('Pi ' + Math.PI);
+getHumanChoice = () => {
+    // Get user input from a prompt
+    let choice = prompt("Enter rock, paper, or scissors:").toLowerCase();
+    // Validate input
+    while (!['rock', 'paper', 'scissors'].includes(choice)) {
+        choice = prompt("Invalid choice. Please enter rock, paper, or scissors:").toLowerCase();
+    }
+    return choice;
+}
+
+playRound = (humanChoice, computerChoice) => {
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        return "You win! Rock beats scissors.";
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        return "You win! Paper beats rock.";
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        return "You win! Scissors beats paper.";
+    }
+    return "You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice + ".";
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
