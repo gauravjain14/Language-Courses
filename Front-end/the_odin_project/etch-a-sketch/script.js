dfee3edd3be8322556be2607e472d71f0db4644d
Gaurav Jain <gaurjain14@gmail.com>
2025-04-22 14:35:29 -0700

    Responsive row and column and hover effect

--- Front-end/the_odin_project/etch-a-sketch/script.js
+++ Front-end/the_odin_project/etch-a-sketch/script.js
@@ -1,20 +1,35 @@
 const container = document.querySelector('.container');
-
-// Create a 16 x 16 grid of square divs
+container.style.flexDirection = "column";
+container.style.height = "400px";
 let grid = document.createElement("div");
 grid.classList.add("grid");
-
 // This is needed to show them side-by-side
 grid.style.display = "flex";
+grid.style.justifyContent = "space-between";
+grid.style.flexDirection = "column";
+grid.style.height = "100%";
 function createGrid() {
     for (let i = 0; i < 16; i++) {
-        let div = document.createElement("div");
-        div.classList.add("column");
-        div.style.flex = 1;
-        div.style.backgroundColor = "lightgray";
-        div.style.height = "20px";
-        div.style.border = "1px solid black";
-        grid.appendChild(div);
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.style.display = "flex";
+        row.style.flex = 1;
+        row.style.justifyContent = "space-between";
+        for (let i = 0; i < 16; i++) {
+            let div = document.createElement("div");
+            div.classList.add("column");
+            div.style.flex = 1;
+            div.style.backgroundColor = "lightgray";
+            div.style.border = "1px solid black";
+            div.onmouseover = function() { 
+                this.style.backgroundColor = "black"; 
+            }
+            div.onmouseout = function() { 
+                this.style.backgroundColor = "lightgray"; 
+            }
+            row.appendChild(div);
+        }
+        grid.appendChild(row);
     }
 }
 
