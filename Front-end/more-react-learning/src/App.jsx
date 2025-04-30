0b0a3c100f489f25e0a3729d92beb82213dc44a9
Gaurav Jain <gaurjain14@gmail.com>
2025-04-29 20:27:34 -0700

    Keep using those hooks

--- Front-end/more-react-learning/src/App.jsx
+++ Front-end/more-react-learning/src/App.jsx
@@ -3,12 +3,55 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './SomeProps';
+import { sculptureList } from './data.js';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [backgroundColor, setBackgroundColor] = useState('red');
+
   const handleButtonClick = () => {
+    setBackgroundColor('blue');
     window.open('https://www.google.com', '_blank');
   }
+
+  
+  function Gallery() {
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+    
+    function handleNextClick() {
+      setIndex((index + 1) % sculptureList.length);
+    }
+  
+    function handleMoreClick() {
+      setShowMore(!showMore);
+    }
+  
+    let sculpture = sculptureList[index];
+    return (
+      <>
+        <button onClick={handleNextClick}>
+          Next
+        </button>
+        <h2>
+          <i>{sculpture.name} </i> 
+          by {sculpture.artist}
+        </h2>
+        <h3>  
+          ({index + 1} of {sculptureList.length})
+        </h3>
+        <button onClick={handleMoreClick}>
+          {showMore ? 'Hide' : 'Show'} details
+        </button>
+        {showMore && <p>{sculpture.description}</p>}
+        <img 
+          src={sculpture.url} 
+          alt={sculpture.alt}
+        />
+      </>
+    );
+  }
+
 
   return (
     <>
@@ -33,8 +76,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <div>
-      <Button label="Click me" color="red" backgroundColor="blue" onClick={handleButtonClick}/>
+      <Button label="Click me" color="red" backgroundColor={backgroundColor} onClick={handleButtonClick}/>
       </div>
+      <Gallery />
     </>
   )
 }
